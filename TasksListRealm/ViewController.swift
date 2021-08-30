09a6a0f6c6c0f4c955b7e262c6a236d4ed77dd05
TDB-star <tatiana.blache@gmail.com>
2021-08-30 18:21:10 +0200

    Initial Commit

---
+++ TasksListRealm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TasksListRealm
+//
+//  Created by Tatiana Dmitrieva on 30/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
